de850c11fefe14b8cb8f36bf82b35db4e163cce7
shradha mantry <itsmehak27@gmail.com>
2024-06-04 02:14:52 +0530

    ellipse-added

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -9,6 +9,8 @@ import Frame3 from '../assets/F3.png';
 import Frame4 from '../assets/F4.png';
 import Img2 from '../assets/img2.png';
 import { IoIosPlayCircle } from "react-icons/io";
+import Ellipse1 from "../assets/Ellipse1.png";
+import Ellipse2 from "../assets/Ellipse2.png";
 
 const Hero = () => {
   const [activeLink, setActiveLink] = useState(null);
@@ -23,7 +25,7 @@ const Hero = () => {
           <img src={Logo} alt="Logo" />
         </div>
         <div className="navbar-links">
-        <a href="#home" className={activeLink === 'home' ? 'active' : ''} onClick={() => handleLinkClick('home')}>Home</a>
+        <a href="#home" className={activeLink === 'home' ? 'active' : ''} onClick={() => handleLinkClick('home')} home>Home</a>
           <a href="#aboutUs" className={activeLink === 'about' ? 'active' : ''} onClick={() => handleLinkClick('about')}>About Us</a>
           <a href="#pricing" className={activeLink === 'pricing' ? 'active' : ''} onClick={() => handleLinkClick('pricing')}>Pricing</a>
           <a href="#Features" className={activeLink === 'features' ? 'active' : ''} onClick={() => handleLinkClick('features')}>Features</a>
@@ -34,6 +36,7 @@ const Hero = () => {
         {/* <img src={Star} className="star-image" alt="Star" /> */}
       </nav>
       <div className="image-container">
+      {/* <img src={Ellipse1} alt="" className='E2' /> */}
         <div className='text'>
           <div className='text1'>Make The Best <br />Financial Decisions</div>
           <div className='text2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam nulla optio<br /> molestiae excepturi  beatae et, enim dolore dicta rem. Voluptatum</div>
@@ -45,6 +48,7 @@ const Hero = () => {
        </div>
           <div><img src={Img2} alt="" className='img2' /></div>
         </div>
+        <img src={Ellipse2} alt="" className='e1'/>
         <img src={Frame1} alt="Frame 1" className="frame1" />
         <img src={Frame2} alt="" className='frame2' />
         <img src={Frame3} alt="" className='frame3'/>
